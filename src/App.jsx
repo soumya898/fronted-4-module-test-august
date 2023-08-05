3e3a65891d82cdd17eacd8834011e94acf3ff472
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-08-06 00:08:14 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,97 @@
+// import './App.css';
+// import {Routes, Route} from 'react-router-dom'
+// import HomePage from './Components/HomePage';
+// import DetailsPage from './Components/DetailsPage';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <nav>
+//         <h1>Social Media  For Travellers </h1>
+//       </nav>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/item/:id" element={<DetailsPage />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import './App.css';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom';
+import { FaHome, FaUser } from 'react-icons/fa'; // Import icons
 import HomePage from './Components/HomePage';
 import DetailsPage from './Components/DetailsPage';
 
 function App() {
+  const navStyles = {
+    backgroundColor: 'orange',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px 20px',
+  };
+
+  const logoStyles = {
+    textDecoration: 'none',
+    color: 'black',
+    fontSize: '20px',
+    fontWeight: 'bold',
+  };
+
+  const linkContainerStyles = {
+    display: 'flex',
+    alignItems: 'center',
+  };
+
+  const linkStyles = {
+    textDecoration: 'none',
+    color: 'black',
+    marginLeft: '20px',
+    fontWeight: 'bold',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    position: 'relative',
+  };
+
+  const iconStyles = {
+    fontSize: '24px', // Adjust the size as needed
+    marginBottom: '5px',
+  };
+
+  const nameStyles = {
+    display: 'none',
+  };
+
+  const linkHoverStyles = {
+    ':hover': {
+      [nameStyles]: {
+        display: 'block',
+      },
+    },
+  };
+
   return (
     <div className="App">
-      <nav>
-        <h1>Social Media App</h1>
+      <nav style={navStyles}>
+        <div>
+          <Link to="/" style={logoStyles}>
+            TravelMedia.in
+          </Link>
+        </div>
+        <div style={{ ...linkContainerStyles, justifyContent: 'center' }}>
+          <Link to="/" style={{ ...linkStyles, ...linkHoverStyles }}>
+            <FaHome style={iconStyles} />
+            <span style={nameStyles}>Home</span>
+          </Link>
+          <Link to="/profile" style={{ ...linkStyles, ...linkHoverStyles }}>
+            <FaUser style={iconStyles} />
+            <span style={nameStyles}>Profile</span>
+          </Link>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<HomePage />} />
--- src/Components/DetailsPage.jsx
+++ src/Components/DetailsPage.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
-import { useSelector } from "react-redux";
+// import React from 'react';
+// import { useSelector } from "react-redux";
+
+// const DetailsPage = () => {
+//   const { post } = useSelector((state) => state.detailsCard);
+  
+//   if(!post) return <h1>Loading...</h1>
+  
+//   return (
+//     <div className='details-container'>
+//       <h2>Details Page For Post With ID {post.id}</h2>
+//       <img src={`https://picsum.photos/200?random=${post.id}`} alt={post.title} />
+//       <p>User ID: {post.userId}</p>
+//       <h3>Title: {post.title}</h3>
+//       <p>Body: {post.body}</p>
+//   </div>
+//   )
+// }
+
+// export default DetailsPage
+
+
+
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const DetailsPage = () => {
   const { post } = useSelector((state) => state.detailsCard);
-  
-  if(!post) return <h1>Loading...</h1>
-  
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserInfo, setShowUserInfo] = useState(false);
+
+  if (!post) return <h1>Loading...</h1>;
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+    setShowUserInfo(false);
+  };
+
+  const toggleUserInfo = () => {
+    setShowUserInfo(!showUserInfo);
+    setShowDetails(false);
+  };
+
   return (
     <div className='details-container'>
       <h2>Details Page For Post With ID {post.id}</h2>
       <img src={`https://picsum.photos/200?random=${post.id}`} alt={post.title} />
-      <p>User ID: {post.userId}</p>
-      <h3>Title: {post.title}</h3>
-      <p>Body: {post.body}</p>
-  </div>
-  )
-}
+      <div style={{ margin: '10px 0' }}>
+        <button onClick={toggleDetails}>Show Details</button>
+        <button onClick={toggleUserInfo}>User Info</button>
+      </div>
+      {showDetails && (
+        <div>
+          <p>User ID: {post.userId}</p>
+          <h3>Title: {post.title}</h3>
+          <p>Body: {post.body}</p>
+        </div>
+      )}
+      {showUserInfo && <p>Post Was Posted By User ID: {post.userId}</p>}
+    </div>
+  );
+};
 
-export default DetailsPage
+export default DetailsPage;
